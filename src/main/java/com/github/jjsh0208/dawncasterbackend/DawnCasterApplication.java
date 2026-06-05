8870791cc474ebc39a4af2fd1a48761c591a5b45
jjsh0208 <jjsh0208@gmail.com>
2026-06-05 18:40:34 +0900

    first commit

---
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/DawnCasterApplication.java
@@ -0,0 +1,13 @@
+package com.github.jjsh0208.dawncasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnCasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnCasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jjsh0208/dawncasterbackend/DawnCasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jjsh0208.dawncasterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnCasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
