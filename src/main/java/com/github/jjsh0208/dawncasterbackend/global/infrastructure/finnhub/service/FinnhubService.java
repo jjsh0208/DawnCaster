93868b13561f83a84c2bf450d826af04a3201af2
jjsh0208 <jjsh0208@gmail.com>
2026-06-08 15:29:47 +0900

    feat : <Finnhub> 3대 증시 정보 예외처리 추가
    - 예외 발생시 null 값 반환 하도록 변경

--- src/main/java/com/github/jjsh0208/dawncasterbackend/global/infrastructure/finnhub/service/FinnhubService.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/global/infrastructure/finnhub/service/FinnhubService.java
@@ -2,29 +2,40 @@ package com.github.jjsh0208.dawncasterbackend.global.infrastructure.finnhub.serv
 
 import com.github.jjsh0208.dawncasterbackend.global.infrastructure.finnhub.dto.QuoteResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FinnhubService {
 
     private final RestClient finnhubRestClient;
 
-
     @Value("${finnhub.api-key}")
     private String apiKey;
 
+    // 미국 3대 증시 정보
     public QuoteResponse getQuote(String symbol) {
 
-        return finnhubRestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/quote")
-                        .queryParam("symbol", symbol)
-                        .queryParam("token", apiKey)
-                        .build())
-                .retrieve()
-                .body(QuoteResponse.class);
+        try {
+            return finnhubRestClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/quote")
+                            .queryParam("symbol", symbol)
+                            .queryParam("token", apiKey)
+                            .build())
+                    .retrieve()
+                    .body(QuoteResponse.class);
+        } catch (RestClientException e) {
+            log.error("[Finnhub API Error] 주가 조회 실패 (심볼: {}): {}", symbol, e.getMessage());
+            return null;
+        }
     }
+
+
+
 }
