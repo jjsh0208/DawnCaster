e33309df6d91b7301aa82a7b811905e9ee6082d1
jjsh0208 <jjsh0208@gmail.com>
2026-06-08 15:44:03 +0900

    feat : <news> 중복된 뉴스인지 검사하기 위한 url 검사 추가

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/news/repository/NewsRepository.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/news/repository/NewsRepository.java
@@ -4,4 +4,5 @@ import com.github.jjsh0208.dawncasterbackend.domain.news.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+    boolean existsByUrl(String url);
 }
