aaaaca34044a17db5cc69cd0c978b018379b7c6a
jjsh0208 <jjsh0208@gmail.com>
2026-06-08 18:29:16 +0900

    feat : <stock> 시장 지수 시황 데이터의 날짜별 중복 저장 방지 제약조건 추가

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/stock/entity/StockMarketSummary.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/stock/entity/StockMarketSummary.java
@@ -10,7 +10,12 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "stock_market_summary")
+@Table(name = "stock_market_summary", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uq_market_date_index",
+                columnNames = {"market_date", "index_name"}
+        )
+})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StockMarketSummary extends BaseTimeEntity {
@@ -20,14 +25,14 @@ public class StockMarketSummary extends BaseTimeEntity {
     private Long id;
 
     @Column(name = "market_date", nullable = false)
-    private LocalDate marketDate;
+    private LocalDate marketDate; //미국 장 마감 날짜
 
     @Column(name = "index_name", nullable = false, length = 100)
-    private String indexName;
+    private String indexName; // 증시 명
 
     @Column(name = "close_price", precision = 15, scale = 2)
-    private BigDecimal closePrice;
+    private BigDecimal closePrice; // 마감 종가
 
     @Column(name = "change_rate", precision = 8, scale = 2)
-    private BigDecimal changeRate;
+    private BigDecimal changeRate; // 전일 대비 등락률 (%)
 }
