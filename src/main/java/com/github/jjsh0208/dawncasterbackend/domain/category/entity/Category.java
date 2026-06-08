9e33dad0c3f68406d5d61ddd9c6d8a328eba94ce
jjsh0208 <jjsh0208@gmail.com>
2026-06-08 15:31:32 +0900

    feat : <category> 칼럼 주석 추가 및 내부 메서드 추가
    - , 로 구분된 카테고리를 분리하는 유틸 메서드 구현

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/category/entity/Category.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/category/entity/Category.java
@@ -6,6 +6,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 @Table(name = "categories")
 @Getter
@@ -17,11 +21,20 @@ public class Category extends BaseTimeEntity {
     private Long id;
 
     @Column(nullable = false, length = 100)
-    private String name;
+    private String name; // 한글 이름
 
     @Column(name = "en_name", nullable = false, length = 100)
-    private String enName;
+    private String enName; // 내부 식별용 영어 이름
 
     @Column(name = "search_keywords", length = 500)
-    private String searchKeywords;
+    private String searchKeywords; // 검색 카테고리
+
+    public List<String> getSymbolList() {
+        if (this.searchKeywords == null || this.searchKeywords.isBlank()) {
+            return List.of();
+        }
+        return Arrays.stream(this.searchKeywords.split(","))
+                .map(String::trim) // 공백 제거 방어 로직
+                .collect(Collectors.toList());
+    }
 }
