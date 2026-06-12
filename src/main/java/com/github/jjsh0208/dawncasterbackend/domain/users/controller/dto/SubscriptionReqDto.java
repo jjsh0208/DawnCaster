5fcdd6eb6aad1b704da8258f54dc937697ee01db
jjsh0208 <jjsh0208@gmail.com>
2026-06-12 18:03:31 +0900

    feat: <common> SubscriptionReqDto validation 제약조건 추가
    - email양식 검사
    - 카테고리 최소 1 개이상 입력

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/users/controller/dto/SubscriptionReqDto.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/users/controller/dto/SubscriptionReqDto.java
@@ -1,10 +1,19 @@
 package com.github.jjsh0208.dawncasterbackend.domain.users.controller.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public record SubscriptionReqDto(
+
+        @NotBlank(message = "이메일은 필수 입력값입니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
+
+        @NotEmpty(message = "최소 하나 이상의 카테고리를 선택해야 합니다.")
         List<Long> categoryIds
 ){
 }
