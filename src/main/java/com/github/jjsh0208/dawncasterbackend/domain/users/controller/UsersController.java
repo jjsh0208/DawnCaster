985f96d298bcbee61223c087350ef402fe986542
jjsh0208 <jjsh0208@gmail.com>
2026-06-12 18:04:05 +0900

    feat: <user> @vild 적용

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/users/controller/UsersController.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/users/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.github.jjsh0208.dawncasterbackend.domain.users.controller;
 
 import com.github.jjsh0208.dawncasterbackend.domain.subscription.service.SubscriptionService;
 import com.github.jjsh0208.dawncasterbackend.domain.users.controller.dto.SubscriptionReqDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class UsersController {
     private final SubscriptionService subscriptionService;
 
     @PostMapping("/subscription")
-    public ResponseEntity<?> subscription(@RequestBody SubscriptionReqDto dto){
+    public ResponseEntity<?> subscription(@Valid @RequestBody SubscriptionReqDto dto){
         subscriptionService.subscribeCategories(dto.email(), dto.categoryIds());
         return ResponseEntity.ok("구독 성공");
     }
