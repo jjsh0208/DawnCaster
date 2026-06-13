0a2d00c6463f5b3f228dc69ed43b779f5f5ad3bb
jjsh0208 <jjsh0208@gmail.com>
2026-06-13 21:45:08 +0900

    feat : <User> Subscription 양방향 매핑 및 연관관계 편의 메서드 추가
    - User와 Subscription 간의 양방향 연관관계(@OneToMany) 매핑 추가
    - 트랜잭션 내 객체 상태 동기화를 위한 addSubscription() 연관관계 편의 메서드 구현
    - 활성화된 구독 카테고리 ID만 추출하여 반환하는 비즈니스 헬퍼 메서드(getSubscribedCategoryIds) 추가

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/users/entity/User.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/users/entity/User.java
@@ -1,11 +1,15 @@
 package com.github.jjsh0208.dawncasterbackend.domain.users.entity;
 
+import com.github.jjsh0208.dawncasterbackend.domain.subscription.entity.Subscription;
 import com.github.jjsh0208.dawncasterbackend.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -23,8 +27,23 @@ public class User extends BaseTimeEntity {
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
 
+    @OneToMany(mappedBy = "user")
+    private List<Subscription> subscriptions = new ArrayList<>();
+
     @Builder
     public User(String email) {
         this.email = email;
+    }
+
+    public void addSubscription(Subscription subscription) {
+        this.subscriptions.add(subscription);
+    }
+
+    // 비즈니스 헬퍼 메서드: 활성화된 구독의 카테고리 ID 목록 추출
+    public List<Long> getSubscribedCategoryIds() {
+        return this.subscriptions.stream()
+                .filter(Subscription::isActive)
+                .map(sub -> sub.getCategory().getId())
+                .toList();
     }
 }
