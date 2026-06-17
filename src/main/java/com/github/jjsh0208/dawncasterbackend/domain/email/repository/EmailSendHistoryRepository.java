dc35b6b2cf6728c5c8bbd8e4bf0aedb974d30747
jjsh0208 <jjsh0208@gmail.com>
2026-06-17 22:21:27 +0900

    feat: <Email> 전송 실패한 메일 조회 메서드 추가
    - 특정 상태(FAIL)와 당일 날짜를 기준으로 발송 이력 조회

--- src/main/java/com/github/jjsh0208/dawncasterbackend/domain/email/repository/EmailSendHistoryRepository.java
+++ src/main/java/com/github/jjsh0208/dawncasterbackend/domain/email/repository/EmailSendHistoryRepository.java
@@ -1,7 +1,13 @@
 package com.github.jjsh0208.dawncasterbackend.domain.email.repository;
 
 import com.github.jjsh0208.dawncasterbackend.domain.email.entity.EmailSendHistory;
+import com.github.jjsh0208.dawncasterbackend.domain.email.enums.SendStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface EmailSendHistoryRepository extends JpaRepository<EmailSendHistory, Long> {
+
+    List<EmailSendHistory> findBySendStatusAndSendDate(SendStatus sendStatus, LocalDate sendDate);
 }
